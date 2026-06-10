b174ed4e9e463516123546a3ad2194410514fdf8
vaileth coder <vailethkimati2020@gmail.com>
2026-06-10 13:59:19 +0300

    Translate Add New Uniform button to English

--- components/ReportsView.tsx
+++ components/ReportsView.tsx
@@ -529,7 +529,7 @@ export function ReportsView() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-3.5 h-3.5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
               </svg>
-              Ongeza Sare Mpya
+              Add New Uniform
             </button>
           </div>
 
